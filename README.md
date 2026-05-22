3b6ccc59928887ef4197cf389d44375b59ee8b2d
clark1492 <76492078+clark1492@users.noreply.github.com>
2026-05-22 17:47:32 +0200

    Ultimo commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Azienda_IT
 sito web dell'azienda IT
+
+# TODO: 
+Migliorare pagina Claudio con un card simile agli altri profili 
--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const dropdownMenu = document.getElementById("menu");
-
-menuButton.addEventListener("click", function () {
-    dropdownMenu.classList.toggle("active");
-});
